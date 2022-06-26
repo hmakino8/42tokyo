61bd12b57408b94c51bd3f08939bf1569034d318
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-06-26 09:50:30 +0900

    fixed return value

--- 42cursus/Libft/ft_atoi.c
+++ 42cursus/Libft/ft_atoi.c
@@ -6,36 +6,36 @@
 /*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 23:44:45 by hiroaki           #+#    #+#             */
-/*   Updated: 2022/06/21 19:24:56 by hmakino          ###   ########.fr       */
+/*   Updated: 2022/06/26 09:49:56 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isspace(int c)
+static int ft_isspace(int c)
 {
 	return (c == ' ' || ('\t' <= c && c <= '\r'));
 }
 
-static int	ft_overflow(int sign)
+static long overflow(int sign)
 {
 	if (sign > 0)
-		return (-1);
-	return (0);
+		return (LONG_MAX);
+	return (LONG_MIN);
 }
 
-static int	ft_isoverflow(unsigned long num, int sign)
+static int is_overflow(unsigned long num, int sign)
 {
-	if ((sign > 0 && num > LONG_MAX) || \
-			(sign < 0 && num > (LONG_MAX + 1UL)))
+	if ((sign > 0 && num > LONG_MAX) ||
+		(sign < 0 && num > (LONG_MAX + 1UL)))
 		return (1);
 	return (0);
 }
 
-int	ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
-	int				sign;
-	unsigned long	num;
+	int sign;
+	unsigned long num;
 
 	sign = 1;
 	num = 0;
@@ -49,8 +49,8 @@ int	ft_atoi(const char *str)
 	while (ft_isdigit(*str))
 	{
 		num = (num * 10) + (*str++ - '0');
-		if (ft_isoverflow(num, sign))
-			return (ft_overflow(sign));
+		if (is_overflow(num, sign))
+			return ((int)overflow(sign));
 	}
 	return ((int)num * sign);
 }
@@ -58,7 +58,7 @@ int	ft_atoi(const char *str)
 //#include <stdio.h>
 //#include <limits.h>
 //
-//int main(void)
+// int main(void)
 //{
 //	printf("***********************************\n");
 //	printf("tab return\natoi    : %d\n", atoi("\t\r-1"));
@@ -79,6 +79,12 @@ int	ft_atoi(const char *str)
 //	printf("INT_MIN - 1\natoi    :%d\n", atoi("-2147483649"));
 //	printf("ft_atoi :%d\n", ft_atoi("-2147483649"));
 //	printf("-----------------------------------\n");
+//	printf("LONG_MAX\natoi    :%d\n", atoi("9223372036854775807"));
+//	printf("ft_atoi :%d\n", ft_atoi("9223372036854775807"));
+//	printf("-----------------------------------\n");
+//	printf("LONG_MIN\natoi    :%d\n", atoi("-9223372036854775808"));
+//	printf("ft_atoi :%d\n", ft_atoi("-9223372036854775808"));
+//	printf("-----------------------------------\n");
 //	printf("LONG_MAX + 1\natoi    :%d\n", atoi("9223372036854775808"));
 //	printf("ft_atoi :%d\n", ft_atoi("9223372036854775808"));
 //	printf("-----------------------------------\n");
@@ -91,3 +97,4 @@ int	ft_atoi(const char *str)
 //
 //	return (0);
 //}
+//
