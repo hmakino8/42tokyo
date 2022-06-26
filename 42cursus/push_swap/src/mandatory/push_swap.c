084497e3d3c3704c78dc1fbbe29ccc036125702e
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-06-27 03:58:59 +0900

    update

--- 42cursus/push_swap/src/mandatory/push_swap.c
+++ 42cursus/push_swap/src/mandatory/push_swap.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmakino <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 17:50:49 by hmakino           #+#    #+#             */
-/*   Updated: 2022/06/21 22:40:59 by hmakino          ###   ########.fr       */
+/*   Updated: 2022/06/26 09:18:13 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-char	*find_separater(int ac, char **av)
+char *find_separater(int ac, char **av)
 {
-	int	locate;
+	int locate;
 
 	locate = (ac - 1) / 2 + 1;
 	return (av[index_asc_order(locate)]);
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	//t_pswap	ps;
+	// t_pswap	ps;
 
 	find_separater();
 	return (0);
