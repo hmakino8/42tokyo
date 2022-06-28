65755cf855704218b87ab9742024ea08e0bc3cd3
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-06-28 09:15:00 +0900

    echo a'abc''def' -> ok

--- 42cursus/pipex/src/mandatory/split.c
+++ 42cursus/pipex/src/mandatory/split.c
@@ -6,7 +6,7 @@
 /*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 17:02:30 by hiroaki           #+#    #+#             */
-/*   Updated: 2022/06/28 03:04:19 by hiroaki          ###   ########.fr       */
+/*   Updated: 2022/06/28 09:12:34 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,47 +19,42 @@ static bool	is_quotation_mark(char c)
 	return (false);
 }
 
-static size_t	element_cnt(char *cmds, t_pipex *px)
+static size_t	element_count(char *cmds, t_pipex *px)
 {
 	size_t	cnt;
 	char	q_mark;
 
-	cnt = 0;
+	cnt = 1;
 	while (*cmds)
 	{
-		if (*cmds++ != ' ')
-		{
-			cnt++;
-			while (*cmds && *cmds != ' ')
-				cmds++;
-		}
 		if (is_quotation_mark(*cmds) == true)
 		{
-			cnt++;
 			q_mark = *cmds++;
-			while (*cmds != q_mark)
+			while (*cmds && *cmds != q_mark)
 				cmds++;
 			cmds++;
 		}
+		else if (*cmds && *cmds != ' ')
+		{
+			while (*cmds && *cmds != ' ' && \
+				is_quotation_mark(*cmds) == false)
+				cmds++;
+		}
+		if (*cmds == ' ')
+			cnt++;
+		while (*cmds && *cmds == ' ')
+			cmds++;
 	}
 	return (cnt);
 }
 
-static void	sub_elems(char *cmds, size_t len, t_pipex *px)
-{
-	px->cmd[px->idx] = ft_substr(cmds, 0, len);
-	if (!(px->cmd[px->idx]))
-		exit_fail(0, NULL, px);
-	px->idx++;
-}
-
-static void	element_cpy(char *cmds, size_t cnt, t_pipex *px)
+static char	*elems_join(char *cmds, t_pipex *px)
 {
 	size_t	len;
+	char	*tmp;
 	char	q_mark;
 
-	px->idx = 0;
-	while (*cmds && px->idx < cnt)
+	while (*cmds && *cmds != ' ')
 	{
 		len = 0;
 		if (is_quotation_mark(*cmds) == true)
@@ -67,17 +62,45 @@ static void	element_cpy(char *cmds, size_t cnt, t_pipex *px)
 			q_mark = *cmds++;
 			while (*(cmds + len) && *(cmds + len) != q_mark)
 				len++;
-			sub_elems(cmds, len, px);
+			tmp = ft_substr(cmds, 0, len);
+			if (!tmp)
+				exit_fail(0, NULL, px);
+			px->cmd[px->idx] = ft_strjoin(px->cmd[px->idx], tmp);
+			free(tmp);
+			if (!px->cmd[px->idx])
+				exit_fail(0, NULL, px);
 			cmds += len + 1;
 		}
-		if (*cmds && *cmds != ' ')
+		else if (*cmds && *cmds != ' ')
 		{
-			while (*(cmds + len) && *(cmds + len) != ' ')
+			while (*(cmds + len) && *(cmds + len) != ' ' && \
+				is_quotation_mark(*(cmds + len)) == false)
 				len++;
-			sub_elems(cmds, len, px);
+			tmp = ft_substr(cmds, 0, len);
+			if (!tmp)
+				exit_fail(0, NULL, px);
+			px->cmd[px->idx] = ft_strjoin(px->cmd[px->idx], tmp);
+			free(tmp);
+			if (!px->cmd[px->idx])
+				exit_fail(0, NULL, px);
 			cmds += len;
 		}
-		cmds++;
+	}
+	return (cmds);
+}
+
+static void	element_copy(char *cmds, size_t cnt, t_pipex *px)
+{
+	px->idx = 0;
+	while (*cmds && px->idx < cnt)
+	{
+		if (*cmds == ' ')
+		{
+			px->idx++;
+			while (*cmds && *cmds == ' ')
+				cmds++;
+		}
+		cmds = elems_join(cmds, px);
 	}
 }
 
@@ -87,10 +110,10 @@ void	split_cmds(char *cmds, t_pipex *px)
 
 	if (!cmds)
 		return ;
-	cnt = element_cnt(cmds, px);
+	cnt = element_count(cmds, px);
 	px->cmd = (char **)malloc(sizeof(char *) * (cnt + 1));
 	if (!px->cmd)
 		exit_fail(0, NULL, px);
 	px->cmd[cnt + 1] = NULL;
-	return (element_cpy(cmds, cnt, px));
+	return (element_copy(cmds, cnt, px));
 }
